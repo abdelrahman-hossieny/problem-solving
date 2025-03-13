5815fb4573ed923e626a81aaf797e3cac932cd64
abdelrahman-hossieny <89545519+abdelrahman-hossieny@users.noreply.github.com>
2025-03-13 15:20:32 +0200

    Update ALL Q code.java

--- Assiut sheets/week 1/ALL Q code.java
+++ Assiut sheets/week 1/ALL Q code.java
@@ -71,6 +71,26 @@ public class Main {
     }
 }
 //=================================================
-// problem:  Digits Summation 
+// problem:  Digits  or alpha(cap - small)
 //=================================================
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char X = s.next().charAt(0);
 
+        if (X >= '0' && X <= '9') {
+            System.out.println("IS DIGIT");
+        } else {
+            System.out.println("ALPHA");
+            if (X >= 'A' && X <= 'Z') {
+                System.out.println("IS CAPITAL");
+            } else {
+                System.out.println("IS SMALL");
+            }
+        }
+    }
+}
+//=================================================
+// problem:  Char
+//=================================================
