d52ffd8f06d30f47da832a79d6805a25236e8ca6
abdelrahman-hossieny <89545519+abdelrahman-hossieny@users.noreply.github.com>
2025-03-14 17:08:20 +0200

    Update ALL Q code.java

--- Assiut sheets/week 1/ALL Q code.java
+++ Assiut sheets/week 1/ALL Q code.java
@@ -92,5 +92,119 @@ public class Main {
     }
 }
 //=================================================
-// problem:  Char
+// problem:  Calculator
+//=================================================
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        String operator = input.replaceAll("[0-9]", "");
+        
+        String[] parts = input.split("[+\\-*/]");
+
+        int A = Integer.parseInt(parts[0]);
+        int B = Integer.parseInt(parts[1]);
+
+        int result = switch (operator) {
+            case "+" -> A + B;
+            case "-" -> A - B;
+            case "*" -> A * B;
+            case "/" -> A / B;
+            default -> 0;
+        };
+        System.out.println(result);
+    }
+}
+
+//=================================================
+// problem:  First digit !
+//=================================================
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        
+        int fnum = Integer.parseInt(String.valueOf(input.charAt(0)));                                                //  ❌look here❌
+        System.out.println("First number: " + fnum); 
+    }
+}
+
+//=================================================
+// problem:  Coordinates of a Point
+//=================================================
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+        if (x > 0 && y > 0) 
+            System.out.println("Q1");
+        else if (x < 0 && y > 0) 
+            System.out.println("Q2");
+        else if (x < 0 && y < 0) 
+            System.out.println("Q3");
+        else if (x > 0 && y < 0) 
+            System.out.println("Q4");
+        else if (y == 0 && x != 0) 
+            System.out.println("Eixo X");
+        else if (x == 0 && y != 0) 
+            System.out.println("Eixo Y");
+        else 
+            System.out.println("Origem");
+    }
+}
+
+//=================================================
+// problem:  Age in Days
+//=================================================
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.close();
+
+        int years = N / 365;
+        int remainingDays = N % 365;
+        int months = remainingDays / 30;
+        int days = remainingDays % 30;
+
+        System.out.println(years + " years");
+        System.out.println(months + " months");
+        System.out.println(days + " days");
+    }
+}
+//=================================================
+// problem:  Interval
+//=================================================
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double X = sc.nextDouble();
+        sc.close();
+
+        if (X >= 0 && X <= 25) {
+            System.out.println("Interval [0,25]");
+        } else if (X > 25 && X <= 50) {
+            System.out.println("Interval (25,50]");
+        } else if (X > 50 && X <= 75) {
+            System.out.println("Interval (50,75]");
+        } else if (X > 75 && X <= 100) {
+            System.out.println("Interval (75,100]");
+        } else {
+            System.out.println("Out of Intervals");
+        }
+    }
+}
+//=================================================
+// problem:  Interval
 //=================================================
