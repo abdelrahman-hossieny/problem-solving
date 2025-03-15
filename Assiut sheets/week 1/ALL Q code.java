46f603fb5315f160a7f30f4f25005dfaa04aeab0
abdelrahman-hossieny <89545519+abdelrahman-hossieny@users.noreply.github.com>
2025-03-15 04:26:05 +0200

    Update ALL Q code.java

--- Assiut sheets/week 1/ALL Q code.java
+++ Assiut sheets/week 1/ALL Q code.java
@@ -206,5 +206,84 @@ public class Main {
     }
 }
 //=================================================
-// problem:  Interval
+// problem:  Sort Numbers
 //=================================================
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int max = Math.max(a, Math.max(b, c));
+        int min = Math.min(a, Math.min(b, c));
+        int mid = a + b + c - max - min;
+
+        System.out.println(min);
+        System.out.println(mid);
+        System.out.println(max);
+
+        System.out.println();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
+//=================================================
+// problem:  Float or int
+//=================================================
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double N = sc.nextDouble();
+
+        int intPart = (int) N;  
+        double decimalPart = N - intPart; 
+
+        if (decimalPart == 0) {
+            System.out.println("int " + intPart);
+        } else {
+            System.out.printf("float %d %.3f%n", intPart, decimalPart);
+        }
+    }
+}
+//=================================================
+// problem: Comparison
+//=================================================
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        String s = sc.next();
+        int b = sc.nextInt();
+
+        if ((s.equals(">") && a > b) || (s.equals("<") && a < b) || (s.equals("==") && a == b)) {
+            System.out.println("Right");
+        } else {
+            System.out.println("Wrong");
+        }
+    }
+}
+//another solution
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        String S = sc.next();
+        int B = sc.nextInt();
+
+        boolean isCorrect = (S.equals("<") && A < B) || (S.equals(">") && A > B) || (S.equals("=") && A == B);
+
+        System.out.println(isCorrect ? "Right" : "Wrong");
+    }
+}
+//=================================================
+// problem: Mathematical Expression
+//=================================================
+
