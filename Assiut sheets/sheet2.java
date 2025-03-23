b8ecec766356938c9429d094035e11f5151efd05
abdelrahman-hossieny <89545519+abdelrahman-hossieny@users.noreply.github.com>
2025-03-23 16:55:58 +0200

    Update sheet2.java

--- Assiut sheets/sheet2.java
+++ Assiut sheets/sheet2.java
@@ -26,3 +26,21 @@ public class Main {
     }
 
 }
+//=========================================
+//problem : e.
+//=========================================
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int [] arr = new int[N];
+        for (int i = 0; i < N ; i++) {
+            arr[i]= sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[N-1]);
+    }
+}
