be35fc14b70c1d4af742df553c6c8b8e43ebe29a
abdelrahman-hossieny <89545519+abdelrahman-hossieny@users.noreply.github.com>
2025-05-03 13:20:03 +0300

    Update sheet3.java

--- Assiut sheets/sheet3.java
+++ Assiut sheets/sheet3.java
@@ -62,4 +62,38 @@ public class Main {
         }
     }
 }
+//--------------------------------------------------
+// problem: L. Max Subarray                ❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌
+//--------------------------------------------------
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            List<Integer> maxValues = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                int max = arr[i];
+                for (int j = i; j < n; j++) {
+                    max = Math.max(max, arr[j]);
+                    maxValues.add(max);
+                }
+            }
+            for (int val : maxValues) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
