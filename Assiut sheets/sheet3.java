41ba9373b542e5a1e5e6af6369085ffeb09fc938
abdelrahman-hossieny <89545519+abdelrahman-hossieny@users.noreply.github.com>
2025-05-08 01:50:05 +0300

    Update sheet3.java

--- Assiut sheets/sheet3.java
+++ Assiut sheets/sheet3.java
@@ -138,4 +138,27 @@ public class Main {
         System.out.println("Yes");
     }
 }
+//--------------------------------------------------
+// problem: O - Fibonacci
+//--------------------------------------------------
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        long [] fib = new int[N + 1];
+
+        fib[1] = 0;  
+        fib[2] = 1;  
+
+        for (int i = 3; i <= N; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        System.out.println(fib[N]);
+    }
+}
 
